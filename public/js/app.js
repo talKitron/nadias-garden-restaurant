643b378064774641778c64d2f009d88515cab1f8
talkitron <talkitron@gmail.com>
2020-09-07 17:37:05 +0200

    Fix NPM cross-platform

--- public/js/app.js
+++ public/js/app.js
@@ -49878,8 +49878,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/vagrant/nadias/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/vagrant/nadias/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\talki\nadias\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\talki\nadias\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
