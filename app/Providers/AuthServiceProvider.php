2263a84c67519f041bda32ba27be318f16eb3897
talkitron <talkitron@gmail.com>
2020-09-09 18:04:43 +0200

    Add MenuEditor.vue, AdminController.php,  views/admin/menu-editor.blade.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -29,5 +29,8 @@ class AuthServiceProvider extends ServiceProvider
 
         Passport::routes();
 
+        Gate::define('edit-menu', function($user){
+            return $user->is_admin;
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,6 @@ Auth::routes(['verify' => true]);
 Route::get('/', 'HomeController@index')->name('home');
 
 Route::get('/categories', 'CategoryController@index');
+
+Route::get('/menu-editor', 'AdminController@menu')->middleware('can:edit-menu');
 //->middleware('verified')
